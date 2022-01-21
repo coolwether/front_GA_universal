4896be9217ce633e2946273260c202a4ea312a96
t3153 <t3153@testritegroup.com>
2022-01-21 15:44:41 +0800

    Auto-generated commit [ci skip]

--- assets/proxy.conf.js
+++ assets/proxy.conf.js
@@ -1,8 +1,15 @@
 var HttpsProxyAgent = require('https-proxy-agent');
 var proxyConfig = [{
   context: '/uploads',
-  target: 'http://localhost:1337',
-  secure: false
+  target: 'http://127.0.0.1:1337',
+  secure: false,
+  "bypass": function (req, res, proxyOptions) {
+    if (req.headers.accept.indexOf("html") !== -1) {
+      console.log("Skipping proxy for browser request.");
+      return "/index.html";
+    }
+    req.headers["X-Custom-Header"] = "yes";
+  }
 }];
 
 function setupForCorporateProxy(proxyConfig) {
